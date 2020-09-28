25534d16f078ae16ea3d283cadffbd92b81e7579
Leo <Leo@10.0.0.121>
2020-09-28 11:26:27 -0300

    Adicionado telas funcionais

---
+++ src/main/java/Jogadas.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Leo
+ */
+public class Jogadas {
+
+    private int jogos;
+
+    public Jogadas(int jogos) {
+        this.jogos = jogos;
+    }
+
+    Jogadas() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public int getJogos() {
+        return jogos;
+    }
+
+    public void setJogos(int jogos) {
+        this.jogos = jogos;
+    }
+
+}
